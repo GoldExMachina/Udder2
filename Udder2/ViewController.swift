f8d8a685a0b019645cd72d524d87a8df46d2a59f
Alexander <alexandersmith.mail@gmail.com>
2023-10-27 16:42:05 +0100

    Initial Commit

---
+++ Udder2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Udder2
+//
+//  Created by Alexander Smith on 27/10/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
